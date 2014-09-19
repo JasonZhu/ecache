431e154e7636306d7e03454f4c2272b2adec94f9
Jason <haishan.zhu@gmail.com>
2014-09-19 10:43:20 +0800

    added Makefile

---
+++ Makefile
@@ -0,0 +1,4 @@
+EUNIT_SUITE ?= ecache_test
+include make/vars.mk
+include make/common.mk
+include make/rebar.mk
---
+++ make/vars.mk
@@ -0,0 +1,4 @@
+BASE_DIR := $(shell pwd)
+REBAR_REPO ?= https://github.com/rebar/rebar.git
+REBAR ?= $(BASE_DIR)/rebar
+DEPS ?= deps
